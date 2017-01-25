6ee524eb9781139d159bd3884ec0ae19aaef74b3
ricen <jwargol@gmail.com>
2017-01-25 21:44:42 +0100

    Add show method and spec

--- connect_four/lib/board.rb
+++ connect_four/lib/board.rb
@@ -1,8 +1,17 @@
 class Board
   attr_accessor :grid
+  BLANK = "\u2610"
 
   def initialize
     @grid = Array.new(6) { Array.new(7) }
+  end
+
+  def show
+
+    @grid.each do |col|
+      col.each { |el| print (!el ? "#{BLANK}" : el) }
+      print "\n"
+    end
   end
 
 end
--- connect_four/spec/board_spec.rb
+++ connect_four/spec/board_spec.rb
@@ -1,10 +1,23 @@
 require 'board'
 
+BLANK = "\u2610"
+
 describe Board do
   let(:board) { Board.new }
 
   it "initialize 6x7 grid" do
     expect(board.grid.size).to eql(6)
     expect(board.grid[0].size).to eql(7)
+  end
+
+  describe '#show' do
+    it "prints empty board" do
+      expect {board.show}.to output("#{BLANK * 7}\n" * 6).to_stdout
+    end
+
+    it "prints non-empty board" do
+      board.grid[0][0] = 'X'
+      expect {board.show}.to output("X#{BLANK * 6}\n"+"#{BLANK * 7}\n" * 5).to_stdout
+    end
   end
 end
